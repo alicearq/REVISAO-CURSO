14059001e789412c76b49a6c633e9e6f94b11809
Alicearq <aliceguilarducci.arq@hotmail.com>
2021-10-30 21:42:32 -0300

    Promises funcionando

---
+++ arquivo.js
@@ -0,0 +1,19 @@
+function funcaoSupercustosa() {
+   return new Promise (function (resolve, reject){
+      //vai rodar de forma assícrona
+      const resultado = 10 + 5
+       resolve( resultado)
+    })
+}   
+
+function esperar(segundos) {
+    return new Promise(function (resolve, reject){
+      setTimeout(resolve, segundos * 1000)
+    })
+}
+
+async function principal () {
+    await esperar(5)
+    console.log('Passou-se 5 segundos!')
+}
+principal()
